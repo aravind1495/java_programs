152699cf4ced1aae1170a4de982f0bac7b113b4d
Aravind <aravindlucky141@gmail.com>
2022-12-20 01:07:07 +0530

    Swap 2 numbers

---
+++ src/fajp/Swap2Numbers.java
@@ -0,0 +1,37 @@
+package fajp;
+
+public class Swap2Numbers {
+	
+	public static void main (String[] args) {
+		
+		//Using 3rd  variable
+		
+		int x = 2;
+		int y = 5;
+		System.out.println("x = "+x+", y = "+y);
+		
+		int temp = 0;
+		
+		temp = x;
+		x  =  y;
+		y = temp;
+		
+		System.out.println("x = "+x+", y = "+y);
+		
+		//Without using 3rd variable
+		
+		x = 2;
+		y = 5;
+		System.out.println("\nx = "+x+", y = "+y);
+		
+		
+		x =  x + y;
+		y = x - y;
+		x = x - y;
+		
+		//y = x+y-(x=y); - in single statement
+				
+		System.out.println("x = "+x+", y = "+y);		
+	}
+
+}
