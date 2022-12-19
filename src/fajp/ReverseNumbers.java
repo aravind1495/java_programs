d77493589a0df38d6ba2d54a66f9cace2e3f4359
Aravind <aravindlucky141@gmail.com>
2022-12-20 01:36:52 +0530

    Reverse Numbers

---
+++ src/fajp/ReverseNumbers.java
@@ -0,0 +1,36 @@
+package fajp;
+
+public class ReverseNumbers {
+
+	public static void main(String[] args) {
+
+		int num = 12345, rev = 0;
+		
+		int rem = 1;
+		
+		while (num != 0) {
+			
+			rem = num % 10;
+			rev = rev*10 + rem; // rev = rev*10  + num%10;
+			num = num / 10;
+			
+		}
+		
+		System.out.println(rev);
+		
+		
+		
+		//Reverse using StringBuffer
+		
+		double rev2 = 1.23; //(OR) can be int too
+		
+		StringBuffer sf = new StringBuffer(String.valueOf(rev2));
+		
+		sf.reverse();
+		
+		System.out.println(sf);
+		
+
+	}
+
+}
